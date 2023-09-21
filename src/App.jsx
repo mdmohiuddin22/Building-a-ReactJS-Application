e8b8cf2adee0517e75c1521da4e23e80344fc9fa
Md.Mohiuddin <saddammd203@gmail.com>
2023-09-22 00:05:42 +0600

    organize code

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,4 @@
 
-// import './App.css'
-
-// function App() {
-  
-
-//   return (
-//     <>
-      
-//     </>
-//   )
-// }
-
-// export default App
-
-
-// import React, { useState } from 'react';
-
-// function App() {
-//   const [position, setPosition] = useState({ top: 50, left: 50 });
-
-//   const moveUp = () => {
-//     setPosition((prevPosition) => ({ ...prevPosition, top: prevPosition.top - 10 }));
-//   };
-
-//   const moveDown = () => {
-//     setPosition((prevPosition) => ({ ...prevPosition, top: prevPosition.top + 10 }));
-//   };
-
-//   const moveLeft = () => {
-//     setPosition((prevPosition) => ({ ...prevPosition, left: prevPosition.left - 10 }));
-//   };
-
-//   const moveRight = () => {
-//     setPosition((prevPosition) => ({ ...prevPosition, left: prevPosition.left + 10 }));
-//   };
-
-//   return (
-//     <div className="min-h-screen flex flex-col justify-center items-center">
-//       <div className="relative w-36 h-36 border border-blue-500 flex items-center justify-center">
-//         <div
-//           className="w-20 h-20 bg-blue-300 absolute"
-//           style={{ top: `${position.top}px`, left: `${position.left}px` }}
-//         ></div>
-//       </div>
-//       <div className="mt-4 flex justify-center">
-//         <div className="flex flex-col">
-//           <button
-//             className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
-//             onClick={moveUp}
-//           >
-//             Move Up
-//           </button>
-//           <button
-//             className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded mt-2"
-//             onClick={moveDown}
-//           >
-//             Move Down
-//           </button>
-//         </div>
-//         <div className="flex flex-col items-center ml-4">
-//           <button
-//             className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
-//             onClick={moveLeft}
-//           >
-//             Move Left
-//           </button>
-//           <button
-//             className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded mt-2"
-//             onClick={moveRight}
-//           >
-//             Move Right
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-
-// src/App.js
 import React, { useState, useEffect } from 'react';
 import Block from './components/Block';
 
@@ -97,14 +13,14 @@ function App() {
         const deltaX = nextPosition.x - position.x;
         const deltaY = nextPosition.y - position.y;
 
-        // Calculate the distance between the current position and the next position
+    
         const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
 
-        // Set the step size (adjust for desired speed)
+      
         const step = 5;
 
         if (distance > step) {
-          // Calculate the new position
+         
           const newX = position.x + (deltaX / distance) * step;
           const newY = position.y + (deltaY / distance) * step;
 
@@ -115,13 +31,13 @@ function App() {
           setIsMoving(false);
         }
       }
-    }, 1000 / 60); // Adjust the speed by changing the interval duration
+    }, 1000 / 60); 
 
     return () => clearInterval(intervalId);
   }, [position, nextPosition, isMoving]);
 
   const moveBlock = (newDirection) => {
-    const step = 100; // Adjust the step size as needed
+    const step = 100;
 
     switch (newDirection) {
       case 'up':
